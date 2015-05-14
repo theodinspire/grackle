2f4ccadc634da581b31882f944955ee5b995ed50
theodinspire <bobryuu@gmail.com>
2015-05-14 12:27:36 -0500

    Background image code working

--- grackle/src/grackle/Grackle.java
+++ grackle/src/grackle/Grackle.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
@@ -22,6 +24,10 @@ public class Grackle {
 	private String password;
 
 	private JFrame frmGrackle;
+	
+	static private Image icon = Toolkit.getDefaultToolkit().getImage("src/resources/grackleicon.jpg");
+	//static private Image backgound = Toolkit.getDefaultToolkit().getImage("src/resources/grackle.jpg");
+	//static private ImageIcon background = new ImageIcon("src/resources/grackle.jpg");
 
 	/**
 	 * Launch the application.
@@ -43,6 +49,7 @@ public class Grackle {
 	 * Create the application.
 	 */
 	public Grackle() {
+		System.out.println(System.getProperty("user.dir"));
 		password = PassGen.getPassword();
 		initialize();
 	}
@@ -51,19 +58,35 @@ public class Grackle {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmGrackle = new JFrame("Erikson Password Generator");
-		frmGrackle.setTitle("Grackle");
+		frmGrackle = new JFrame("Grackle");
+		frmGrackle.setResizable(false);
 		frmGrackle.setBounds(100, 100, 450, 150);
 		frmGrackle.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGrackle.setIconImage(icon);
 		
-		JPanel panel = new JPanel();
-		frmGrackle.getContentPane().add(panel, BorderLayout.SOUTH);
-		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		//	Background panel for image
+		GrackleMainJPanel mainPanel = new GrackleMainJPanel();
+		mainPanel.setLayout(new BorderLayout());
+		frmGrackle.getContentPane().add(mainPanel, BorderLayout.CENTER);
+		/**************************************************************/
+		
+		//	Panel for displaying password
+		JPanel PassPanel = new JPanel();
+		PassPanel.setOpaque(false);
+		mainPanel.add(PassPanel, BorderLayout.CENTER);
+		PassPanel.setLayout(new GridLayout(0, 1, 0, 0));
 		
 		JLabel lblPassword = new JLabel(password);
+		PassPanel.add(lblPassword);
 		lblPassword.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
-		frmGrackle.getContentPane().add(lblPassword, BorderLayout.CENTER);
+		/********************************************************/
+		
+		//	Button panel
+		JPanel optionPanel = new JPanel();
+		optionPanel.setOpaque(false);
+		mainPanel.add(optionPanel, BorderLayout.SOUTH);
+		optionPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
 		JButton btnCopyToClipboard = new JButton("Copy to Clipboard");
 		btnCopyToClipboard.addActionListener(new ActionListener() {
@@ -71,7 +94,7 @@ public class Grackle {
 				copyToClipboard();
 			}
 		});
-		panel.add(btnCopyToClipboard);
+		optionPanel.add(btnCopyToClipboard);
 		
 		JButton btnGenerateNew = new JButton("Generate New");
 		btnGenerateNew.addActionListener(new ActionListener() {
@@ -80,7 +103,7 @@ public class Grackle {
 				lblPassword.setText(password);
 			}
 		});
-		panel.add(btnGenerateNew);
+		optionPanel.add(btnGenerateNew);
 		
 		JButton btnClose = new JButton("Close");
 		btnClose.addActionListener(new ActionListener() {
@@ -89,7 +112,7 @@ public class Grackle {
 				frmGrackle.dispatchEvent(new WindowEvent(frmGrackle, WindowEvent.WINDOW_CLOSING));
 			}
 		});
-		panel.add(btnClose);
+		optionPanel.add(btnClose);
 	}
 	
 	private void copyToClipboard() {
