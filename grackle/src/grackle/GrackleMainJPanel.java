2982ffb42bc9587524882d9a161533f44f53c557
theodinspire <bobryuu@gmail.com>
2015-06-19 16:36:53 -0500

    Goddammit, why can't I get the images in properly?

--- grackle/src/grackle/GrackleMainJPanel.java
+++ grackle/src/grackle/GrackleMainJPanel.java
@@ -9,7 +9,8 @@ import javax.swing.JPanel;
 public class GrackleMainJPanel extends JPanel {
 	private static final long serialVersionUID = 3724992122744789847L;
 	
-	private Image background = Toolkit.getDefaultToolkit().createImage("src/resources/grackle.jpg");
+	private Image background = Toolkit.getDefaultToolkit().createImage(getClass().getResource("resources/images/grackle.jpg"));
+			//(getClass().getResource("/resources/images/grackle.jpg"));
 	
 	@Override
 	protected void paintComponent(Graphics g) {
