57c383bd626acbd08d2efc7d153ca47a9c8888fd
theodinspire <bobryuu@gmail.com>
2015-06-19 16:52:24 -0500

    Trying my best to get the images in there

--- grackle/src/grackle/GrackleMainJPanel.java
+++ grackle/src/grackle/GrackleMainJPanel.java
@@ -9,7 +9,7 @@ import javax.swing.JPanel;
 public class GrackleMainJPanel extends JPanel {
 	private static final long serialVersionUID = 3724992122744789847L;
 	
-	private Image background = Toolkit.getDefaultToolkit().createImage(getClass().getResource("resources/images/grackle.jpg"));
+	private Image background = Toolkit.getDefaultToolkit().createImage(Grackle.class.getResource("resources/images/grackle.jpg"));
 			//(getClass().getResource("/resources/images/grackle.jpg"));
 	
 	@Override
